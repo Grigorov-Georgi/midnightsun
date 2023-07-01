71b22c597708af1480840c09f2dcaecec786792a
ggrigorov <g.grigorov@blubito.com>
2023-07-01 21:17:37 +0300

    Added notification service

---
+++ noitification-service/src/main/java/com/midnightsun/noitificationservice/NoitificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.midnightsun.noitificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoitificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoitificationServiceApplication.class, args);
+	}
+
+}
---
+++ noitification-service/src/test/java/com/midnightsun/noitificationservice/NoitificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midnightsun.noitificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoitificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
