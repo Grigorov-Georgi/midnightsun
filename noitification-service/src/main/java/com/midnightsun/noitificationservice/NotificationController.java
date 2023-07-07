129cd9cf348cc0403b95d76066b84f978298e0e3
ggrigorov <g.grigorov@blubito.com>
2023-07-07 18:57:04 +0300

    successfully integrated mailhog
    Added simple html template for "created order" scenario

--- noitification-service/src/main/java/com/midnightsun/noitificationservice/NotificationController.java
+++ noitification-service/src/main/java/com/midnightsun/noitificationservice/NotificationController.java
@@ -1,25 +1,51 @@
 package com.midnightsun.noitificationservice;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api")
 public class NotificationController {
 
-    private final EmailService emailService;
+    @RequestMapping("/sendemail/")
+    public String sendEmail() throws MessagingException, IOException {
+        var mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("127.0.0.1");
+        mailSender.setPort(1025);
+        mailSender.setUsername("");
+        mailSender.setPassword("");
 
-    public NotificationController(EmailService emailService) {
-        this.emailService = emailService;
+        var props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+
+        var mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+        mimeMessageHelper.setFrom("system@mail.com");
+        mimeMessageHelper.setTo("g.grigorov@blubito");
+        mimeMessageHelper.setSubject("Order is created");
+        mimeMessageHelper.setText(getHtmlContent("mail-templates/created-order.html"), true);
+
+        mailSender.send(mimeMessage);
+        return "Email sent!";
     }
 
-    @GetMapping("/send/{receiver}")
-    public ResponseEntity<Void> sendMail(@PathVariable("receiver") String receiver) {
-        emailService.sendSimpleMessage(receiver, "that works", "surprise!");
-        return ResponseEntity.status(HttpStatus.OK).build();
+    private String getHtmlContent(String htmlFilePath) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(htmlFilePath)) {
+            if (inputStream != null) {
+                return StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            } else {
+                throw new IllegalArgumentException("HTML file not found: " + htmlFilePath);
+            }
+        }
     }
 }
