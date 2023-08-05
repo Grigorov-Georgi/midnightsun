abc200d881c9895de3bf51c9e924ebe9d589fa26
Lenko Ivanov <lenko.ivanov117@gmail.com>
2023-08-05 14:45:15 +0300

    feat: add first css variables + new custom navbar

--- frontend-client/src/App.tsx
+++ frontend-client/src/App.tsx
@@ -1,24 +1,11 @@
-import "./App.css";
-import BasicCarousel from "./shared_components/Carousel/Carousel";
 import Footer from "./shared_components/Fragments/Footer/Footer";
-import Navbar from "./shared_components/Fragments/Navbar/Navbar";
+import { Navbar } from "./shared_components/Fragments/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-
-      {/* //Home page */}
-      <BasicCarousel />
-
-      {/* //Add Product - Edit Optional */}
-      {/* <ProductForm /> */}
-
-      {/* //List All Products - Catalog */}
-      {/* <ProductGrid /> */}
-
-      {/* //Product Details */}
-      {/* <ProductDetails /> */}
+      MainContent
       <Footer />
     </div>
   );
