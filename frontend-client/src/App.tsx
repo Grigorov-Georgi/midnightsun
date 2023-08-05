d3d289cc1e092a85e9d7a8ba3d2fb4c9c0d25b88
Lenko Ivanov <lenko.ivanov117@gmail.com>
2023-08-05 14:53:43 +0300

    feat: add basic routing

--- frontend-client/src/App.tsx
+++ frontend-client/src/App.tsx
@@ -1,11 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Footer from "./shared_components/Fragments/Footer/Footer";
 import { Navbar } from "./shared_components/Fragments/Navbar/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/productCreation",
+    element: <div>Product creation page</div>,
+  },
+  {
+    path: "/products",
+    element: <div>Products</div>,
+  },
+  {
+    path: "/contacts",
+    element: <div>Contacts</div>,
+  },
+  {
+    path: "/shoppingCart",
+    element: <div>Shopping cart</div>,
+  },
+]);
 
 function App() {
   return (
     <div>
       <Navbar />
-      MainContent
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
