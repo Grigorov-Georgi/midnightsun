6a757b7691357f2db278d1a63d617203195dff46
Lenko Ivanov <lenko.ivanov117@gmail.com>
2023-08-13 19:13:35 +0300

    feat: fetch categories and load dropdown

--- frontend-client/src/services/CategoryService.ts
+++ frontend-client/src/services/CategoryService.ts
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8082/api/categories/";
 
 export const getAllCategories = () => {
-  return axios.get(`${API_BASE_URL}`);
+  // const fetchedCategories: Category[] = [];
+  return axios
+    .get(`${API_BASE_URL}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
 };
