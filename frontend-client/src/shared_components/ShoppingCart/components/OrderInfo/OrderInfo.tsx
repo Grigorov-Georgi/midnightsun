8d8ed4bb1751b60c8ec1a205de59f78e17efacf8
Lenko Ivanov <lenko.ivanov117@gmail.com>
2023-08-23 23:11:13 +0300

    feat: style the order info section

--- frontend-client/src/shared_components/ShoppingCart/components/OrderInfo/OrderInfo.tsx
+++ frontend-client/src/shared_components/ShoppingCart/components/OrderInfo/OrderInfo.tsx
@@ -1,7 +1,14 @@
 import styles from "./OrderInfo.module.scss";
 
 export const OrderInfo = () => {
-  return <div className={styles.infoSection}>Order Info</div>;
+  return (
+    <div className={styles.infoSection}>
+      <span>Total price: </span>
+      <h2>100.00$</h2>
+      <span>Shipping method: Courier</span>
+      <span>Payment: Upon delivery</span>
+    </div>
+  );
 };
 
 export default OrderInfo;
