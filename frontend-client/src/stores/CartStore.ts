496718b7c96737c8b89c93343d48770d4c4e0de3
Lenko Ivanov <lenko.ivanov117@gmail.com>
2023-08-26 17:32:43 +0300

    refactor: make the store persist between route switches

--- frontend-client/src/stores/CartStore.ts
+++ frontend-client/src/stores/CartStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { IOrderItem } from "../types/IOrderItem";
 
 const dummyProducts: IOrderItem[] = [
@@ -21,44 +22,51 @@ interface CartStore {
   calculateTotalPrice: () => void;
 }
 
-export const useCartStore = create<CartStore>((set, get) => ({
-  orderItems: dummyProducts,
-  totalPrice: 0,
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      orderItems: dummyProducts,
+      totalPrice: 0,
 
-  addOrderItem: (newItem: IOrderItem) => {
-    const allItems: IOrderItem[] = get().orderItems;
-    allItems.push(newItem);
-    set(() => ({ orderItems: [...allItems] }));
-    get().calculateTotalPrice();
-  },
+      addOrderItem: (newItem: IOrderItem) => {
+        const allItems: IOrderItem[] = get().orderItems;
+        allItems.push(newItem);
+        set(() => ({ orderItems: [...allItems] }));
+        get().calculateTotalPrice();
+      },
 
-  removeOrderItem: (productId: number) => {
-    const allItems = get().orderItems;
-    const idxToDelete = allItems.findIndex(
-      (item) => item.info.id === productId
-    );
-    allItems.splice(idxToDelete, 1);
-    set(() => ({ orderItems: [...allItems] }));
-    get().calculateTotalPrice();
-  },
+      removeOrderItem: (productId: number) => {
+        const allItems = get().orderItems;
+        const idxToDelete = allItems.findIndex(
+          (item) => item.info.id === productId
+        );
+        allItems.splice(idxToDelete, 1);
+        set(() => ({ orderItems: [...allItems] }));
+        get().calculateTotalPrice();
+      },
 
-  // Incr/decr quantity and recalculate pr
-  modifyOrderItem: (productId: number, newQty: number) => {
-    const allItems = get().orderItems;
-    const selectedItem = allItems.find((item) => item.info.id === productId);
-    if (!selectedItem) return;
-    selectedItem.quantity = newQty;
-    set(() => ({ orderItems: [...allItems] }));
-    get().calculateTotalPrice();
-  },
+      // Incr/decr quantity and recalculate pr
+      modifyOrderItem: (productId: number, newQty: number) => {
+        const allItems = get().orderItems;
+        const selectedItem = allItems.find(
+          (item) => item.info.id === productId
+        );
+        if (!selectedItem) return;
+        selectedItem.quantity = newQty;
+        set(() => ({ orderItems: [...allItems] }));
+        get().calculateTotalPrice();
+      },
 
-  // Needed instead of a reaction (no native computed)
-  calculateTotalPrice: () => {
-    const currentItems: IOrderItem[] = get().orderItems;
-    let totalPrice = 0;
-    currentItems.forEach(
-      (item) => (totalPrice += item.info.price * item.quantity)
-    );
-    set(() => ({ totalPrice: totalPrice }));
-  },
-}));
+      // Needed instead of a reaction (no native computed)
+      calculateTotalPrice: () => {
+        const currentItems: IOrderItem[] = get().orderItems;
+        let totalPrice = 0;
+        currentItems.forEach(
+          (item) => (totalPrice += item.info.price * item.quantity)
+        );
+        set(() => ({ totalPrice: totalPrice }));
+      },
+    }),
+    { name: "cart-storage", storage: createJSONStorage(() => sessionStorage) }
+  )
+);
