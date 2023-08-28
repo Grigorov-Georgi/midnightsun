21f5377cfa46ef91d03640228ef4eff043c8ef34
LenkoIvanov17 <l.ivanov@blubito.com>
2023-08-28 14:28:24 +0300

    feat: add basic error handling in the app

--- frontend-client/src/shared_components/ErrorComponent/ErrorComponent.tsx
+++ frontend-client/src/shared_components/ErrorComponent/ErrorComponent.tsx
@@ -1,12 +1,24 @@
 import styles from "./ErrorComponent.module.css";
 
-export const ErrorComponent = () => {
+export const ErrorComponent = (props: { isPathRelated: boolean }) => {
+  const { isPathRelated = false } = props;
   // TODO -> readjust responsivness later
   return (
     <div className={styles.errorDiv}>
-      <h1>Ooops!</h1>
-      <h2>Your page can not be found! Try a different one!</h2>
-      <h1>404</h1>
+      {isPathRelated && (
+        <>
+          <h1>Ooops!</h1>
+          <h2>Your page can not be found! Try a different one!</h2>
+          <h1>404</h1>{" "}
+        </>
+      )}
+      {!isPathRelated && (
+        <>
+          <h1>Ooops!</h1>
+          <h2>An internal error has occured</h2>
+          <h1>Apologies</h1>
+        </>
+      )}
     </div>
   );
 };
