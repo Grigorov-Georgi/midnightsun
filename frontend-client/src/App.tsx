19d075b19fe618263f1e2e3d5876f7bddd63c6e3
LenkoIvanov17 <l.ivanov@blubito.com>
2023-09-02 19:16:28 +0300

    feat: add axios interceptor for the bearer token

--- frontend-client/src/App.tsx
+++ frontend-client/src/App.tsx
@@ -11,6 +11,7 @@ import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useAuthStore } from "./stores/AuthStore";
 import LoadingOverlay from "./shared_components/LoadingOverlay/LoadingOverlay";
+import axios from "axios";
 
 const router = createBrowserRouter([
   {
@@ -48,6 +49,7 @@ const queryClient = new QueryClient();
 function App() {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
   const refreshToken = useAuthStore((state) => state.refreshToken);
+  const token = useAuthStore((state) => state.token);
 
   useEffect(() => {
     const fetchAccessToken = async () => {
@@ -61,6 +63,16 @@ function App() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getAccessTokenSilently, isAuthenticated]);
+
+  axios.interceptors.request.use(
+    (config) => {
+      config.headers["Authorization"] = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 
   return (
     <div>
