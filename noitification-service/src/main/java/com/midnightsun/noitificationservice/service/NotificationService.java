6f7f8efd90a7a81bfc84070d1dd64cc657f7a1a1
ggrigorov <g.grigorov@blubito.com>
2023-09-03 11:18:18 +0300

    Added jib and started implementing general docker-compose

--- noitification-service/src/main/java/com/midnightsun/noitificationservice/service/NotificationService.java
+++ noitification-service/src/main/java/com/midnightsun/noitificationservice/service/NotificationService.java
@@ -2,6 +2,7 @@ package com.midnightsun.noitificationservice.service;
 
 import com.midnightsun.noitificationservice.service.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -9,42 +10,47 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Slf4j
 @Service
 public class NotificationService {
 
+    private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
 
-    public NotificationService(TemplateEngine templateEngine) {
+    public NotificationService(JavaMailSender mailSender, TemplateEngine templateEngine) {
+        this.mailSender = mailSender;
         this.templateEngine = templateEngine;
     }
 
-    public void sendEmail(OrderDTO order) throws MessagingException {
+    public void sendEmail(OrderDTO order) {
         log.debug("Sending email to {} for creation of ORDER {}", order.getCreatedBy(), order.getId());
 
+        String htmlContent = generateHtml(order);
+
+        try {
+            mailSender.send(generateMessage(htmlContent));
+        } catch (MessagingException e) {
+            log.error("Error during sending an email: {}", e);
+        }
+    }
+
+    private String generateHtml(OrderDTO orderDTO) {
         Context context = new Context();
-        context.setVariable("order", order);
-        context.setVariable("totalPrice", "123");
-        String processedHTML = templateEngine.process("created-order.html", context);
+        context.setVariable("order", orderDTO);
+        return templateEngine.process("created-order.html", context);
+    }
 
-        var mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("127.0.0.1");
-        mailSender.setPort(1025);
-        mailSender.setUsername("");
-        mailSender.setPassword("");
-
-        var props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-
+    private MimeMessage generateMessage(String htmlContent) throws MessagingException {
         var mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
         mimeMessageHelper.setFrom("system@mail.com");
         mimeMessageHelper.setTo("g.grigorov@blubito");
         mimeMessageHelper.setSubject("Order is created");
-        mimeMessageHelper.setText(processedHTML, true);
+        mimeMessageHelper.setText(htmlContent, true);
 
-        mailSender.send(mimeMessage);
+        return mimeMessage;
     }
 }
