e205f55507b890771e83c4b0f978c99acb24aa2d
Georgi Grigorov <72125420+Grigorov-Georgi@users.noreply.github.com>
2023-09-07 20:22:56 +0300

    :art: Made cosmetic change (#24)
    Co-authored-by: ggrigorov <g.grigorov@blubito.com>

--- noitification-service/src/main/java/com/midnightsun/noitificationservice/service/NotificationService.java
+++ noitification-service/src/main/java/com/midnightsun/noitificationservice/service/NotificationService.java
@@ -3,7 +3,6 @@ package com.midnightsun.noitificationservice.service;
 import com.midnightsun.noitificationservice.service.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
@@ -27,30 +26,48 @@ public class NotificationService {
     public void sendEmail(OrderDTO order) {
         log.debug("Sending email to {} for creation of ORDER {}", order.getCreatedBy(), order.getId());
 
-        String htmlContent = generateHtml(order);
-
         try {
-            mailSender.send(generateMessage(htmlContent));
+            mailSender.send(generateMessage(order));
         } catch (MessagingException e) {
             log.error("Error during sending an email: {}", e);
         }
+    }
+
+    private MimeMessage generateMessage(OrderDTO order) throws MessagingException {
+
+        var mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+        mimeMessageHelper.setFrom("system@mail.com");
+
+        String receiver = order.getCustomerEmail();
+        mimeMessageHelper.setTo(receiver);
+
+        String subject;
+        switch (order.getStatus()) {
+            case APPROVED:
+                subject = String.format("Order [%s] was successfully created", order.getId());
+                break;
+            case PENDING:
+                subject = String.format("Order [%s] is registered", order.getId());
+                break;
+            case CANCELED:
+                subject = String.format("Order [%s] is canceled", order.getId());
+                break;
+            default:
+                subject = String.format("Order [%s] information", order.getId());
+        }
+        mimeMessageHelper.setSubject(subject);
+
+        String htmlContent = generateHtml(order);
+        mimeMessageHelper.setText(htmlContent, true);
+
+        return mimeMessage;
     }
 
     private String generateHtml(OrderDTO orderDTO) {
         Context context = new Context();
         context.setVariable("order", orderDTO);
         return templateEngine.process("created-order.html", context);
-    }
-
-    private MimeMessage generateMessage(String htmlContent) throws MessagingException {
-        var mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-
-        mimeMessageHelper.setFrom("system@mail.com");
-        mimeMessageHelper.setTo("g.grigorov@blubito");
-        mimeMessageHelper.setSubject("Order is created");
-        mimeMessageHelper.setText(htmlContent, true);
-
-        return mimeMessage;
     }
 }
