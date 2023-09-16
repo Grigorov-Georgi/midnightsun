f616f2f3c72ee1bd35ed324cec441c5444afc07b
ggrigorov <g.grigorov@blubito.com>
2023-09-16 11:33:58 +0300

    Modified .gitignore

--- eDocker/ecart/scripts/docker-compose-start.sh
+++ eDocker/ecart/scripts/docker-compose-start.sh
@@ -20,5 +20,7 @@ export RRS_DATABASE_USER=${OS_DATABASE_USER}
 export RRS_DATABASE_PASS=${OS_DATABASE_PASS}
 export RRS_DATABASE_URL="jdbc:postgresql://review-rating-service-posgresql:5432/${RRS_DATABASE_NAME}"
 
+export
+
 docker-compose build --pull
 docker-compose up -d --force-recreate
